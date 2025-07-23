527b049a20db30f2daadeb7bf0606fedfd00ba23
priyankavagadre <priyankavagadre@hotmail.com>
2025-07-23 12:01:18 +0530

    use router in index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const app = express();
 
 const PORT =8001;
 
+const urlRouter = require('./routers/url')
+
+app.use('/url', urlRouter)
+
 app.listen(PORT, ()=> console.log('Server started ' +`${PORT}`))
